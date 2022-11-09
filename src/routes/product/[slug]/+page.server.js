12084ce102834ed1e130dab3ce1f5a061418b878
Jamaal <link0047@gmail.com>
2022-11-09 10:02:13 -0500

    remove env test

--- src/routes/product/[slug]/+page.server.js
+++ src/routes/product/[slug]/+page.server.js
@@ -1,8 +1,5 @@
 import parser from 'ua-parser-js';
 import _data from './products.json';
-import { TMDB_API_KEY } from '$env/static/private';
-
-console.log(TMDB_API_KEY);
 
 export function load(data) {
   const ua = parser(data.locals.ua);
